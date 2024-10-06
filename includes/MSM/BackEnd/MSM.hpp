c6cad81bcd4924226d00e61ce944e644445b61b4
K1ngmar <k1ngmar.github@gmail.com>
2024-10-06 15:56:05 +0200

    some experimentation

--- includes/MSM/BackEnd/MSM.hpp
+++ includes/MSM/BackEnd/MSM.hpp
@@ -3,18 +3,21 @@
 
 #include <tuple>
 
-template<std::size_t I = 0, typename FunctionType, typename... TupleTypes>
+template<class OriginState, class Event, class DestinationState, class TransitionFunction, class Guard>
+using TableRow = std::tuple<OriginState, Event, DestinationState, TransitionFunction, Guard>;
+
+template<typename FunctionType, typename... TupleTypes>
 inline typename std::enable_if<I == sizeof...(TupleTypes), void>::type
 for_each_row(std::tuple<TupleTypes...> &, FunctionType)
 {
 
 }
 
-template<std::size_t I = 0, typename FunctionType, typename... TupleTypes>
+template<typename FunctionType, typename... TupleTypes>
 inline typename std::enable_if<I < sizeof...(TupleTypes), void>::type
-for_each_row(std::tuple<TupleTypes...>& t, FunctionType f)
+for_each_row(cons TableRow& t, FunctionType f)
 {
-	f(std::get<I>(t));
+	f(t);
 	for_each_row<I + 1, FunctionType, TupleTypes...>(t, f);
 }
 
@@ -24,22 +27,9 @@ for_each_row(std::tuple<TupleTypes...>& t, FunctionType f)
  * When you intheirit this class make sure you:
  * -
 */
+template<class ...Rows>
 struct MSM
 {
-	template<class OriginState, class Event, class DestinationState, class TransitionFunction, class Guard>
-	using TableRow = std::tuple<OriginState, Event, DestinationState, TransitionFunction, Guard>;
-
-	template<class ...Rows>
-	class TransitionTable
-	{
-		std::tuple<Rows...> table;
-
-		TransitionTable()
-		{
-			
-		}
-	}
-
 	template <class Event>
 	void ProcessEvent(const Event& event)
 	{
@@ -49,7 +39,7 @@ struct MSM
 	template<class CurrentState, class Event>
 	void TransitionState(const CurrentState& currentState, const Event& incommingEvent)
 	{
-		for_each_row<Rows...>([&](const TableRow& row)
+		for_each_row([&](const TableRow& row, Rows...)
 		{
 			constexpr auto& [originState, event, destinationSate, transitionFunction, guard] = row;
 		
@@ -58,7 +48,7 @@ struct MSM
 				if (!guard || guard())
 				{
 					SetStateActive(destinationState);
-					TransitionFunction();
+					transitionFunction();
 					SetStateInactive(originState);
 				}
 			}
