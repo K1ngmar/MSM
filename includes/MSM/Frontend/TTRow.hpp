1bcdea097f9744571b1fab7e71b523d1cd14f45c
K1ngmar <ingmarsven.kole@gmail.com>
2024-10-13 18:09:42 +0200

    fix: row specialisation

--- includes/MSM/Frontend/TTRow.hpp
+++ includes/MSM/Frontend/TTRow.hpp
@@ -12,42 +12,6 @@ namespace Front
     {
     };
 
-    template<class ...T>
-    struct TTRow{};
-
-    // Row<Origin, Event, Target, None, None>
-    template<class OriginState, class Event, class TargetState, class Action, class Guard>
-    struct TTRow<OriginState, Event, TargetState, None, None>
-    {
-        using Row_Type_Tag = Normal_Transition_Tag;
-    };
-
-    // Row<Origin, Event, Target, Action, None>
-    template<class OriginState, class Event, class TargetState, class Action, class Guard>
-    struct TTRow<OriginState, Event, TargetState, Action, None>
-    {
-        using Row_Type_Tag = Normal_Transition_With_Action_Tag;
-
-        template <class Fsm>
-        static void ExecuteAction(Fsm& fsm, const Event& evt, TargetState& tgs)
-        {
-            Action()(fsm, evt, tgs);
-        }
-    };
-
-    // Row<Origin, Event, Target, None, Guard>
-    template<class OriginState, class Event, class TargetState, class Action, class Guard>
-    struct TTRow<OriginState, Event, TargetState, None, Guard>
-    {
-        using Row_Type_Tag = Normal_Transition_With_Guard_Tag;
-
-        template <class Fsm>
-        static bool ExecuteGuard(Fsm& fsm, const Event& evt, TargetState& tgs)
-        {
-            return Guard()(fsm, evt, tgs);
-        }
-    };
-
     // Row<Origin, Event, Target, Action, Guard>
     template<class OriginState, class Event, class TargetState, class Action, class Guard>
     struct TTRow<OriginState, class Event, class TargetState, class Action, class Guard>
@@ -67,8 +31,41 @@ namespace Front
         }
     };
 
+    // Row<Origin, Event, Target, None, None>
+    template<class OriginState, class Event, class TargetState>
+    struct TTRow<OriginState, Event, TargetState, None, None>
+    {
+        using Row_Type_Tag = Normal_Transition_Tag;
+    };
+
+    // Row<Origin, Event, Target, Action, None>
+    template<class OriginState, class Event, class TargetState, class Action>
+    struct TTRow<OriginState, Event, TargetState, Action, None>
+    {
+        using Row_Type_Tag = Normal_Transition_With_Action_Tag;
+
+        template <class Fsm>
+        static void ExecuteAction(Fsm& fsm, const Event& evt, TargetState& tgs)
+        {
+            Action()(fsm, evt, tgs);
+        }
+    };
+
+    // Row<Origin, Event, Target, None, Guard>
+    template<class OriginState, class Event, class TargetState, class Guard>
+    struct TTRow<OriginState, Event, TargetState, None, Guard>
+    {
+        using Row_Type_Tag = Normal_Transition_With_Guard_Tag;
+
+        template <class Fsm>
+        static bool ExecuteGuard(Fsm& fsm, const Event& evt, TargetState& tgs)
+        {
+            return Guard()(fsm, evt, tgs);
+        }
+    };
+
     // Row<Origin, Event, None, Action, None>
-    template<class OriginState, class Event, class TargetState, class Action, class Guard>
+    template<class OriginState, class Event, class Action>
     struct TTRow<OriginState, Event, None, Action, None>
     {
         using Row_Type_Tag = Internal_Transition_Tag;
@@ -81,7 +78,7 @@ namespace Front
     };
 
     // Row<Origin, Event, None, Action, Guard>
-    template<class OriginState, class Event, class TargetState, class Action, class Guard>
+    template<class OriginState, class Event, class Action, class Guard>
     struct TTRow<OriginState, Event, None, Action, Guard>
     {
         using Row_Type_Tag = Internal_Transition_With_Guard_Tag;
@@ -100,7 +97,7 @@ namespace Front
     };
 
     // Row<Origin, Event, None, None, Guard>
-    template<class OriginState, class Event, class TargetState, class Action, class Guard>
+    template<class OriginState, class Event, class Guard>
     struct TTRow<OriginState, Event, None, None, Guard>
     {
         using Row_Type_Tag = Empty_Transition_With_Guard_Tag;
@@ -113,7 +110,7 @@ namespace Front
     };
 
     // Row<Origin, Event, None, None, None>
-    template<class OriginState, class Event, class TargetState, class Action, class Guard>
+    template<class OriginState, class Event>
     struct TTRow<OriginState, Event, None, None, None>
     {
         using Row_Type_Tag = Empty_Transition_Tag;
--- main.cpp
+++ main.cpp
@@ -3,104 +3,23 @@
 #include <iostream>
 #include <tuple>
 
-template<size_t I, typename FunctionType, class ...Rows>
-inline typename std::enable_if<I == sizeof...(Rows), void>::type
-for_each_row(FunctionType) // empty implementation
+struct None
+{};
+
+template<class A, class B, class C>
+struct Row
+{};
+
+template<class A, class C>
+struct Row<A, None, C>
 {
-    std::cout << "empty implementation" << std::endl;
-}
-
-template<size_t I = 0, typename FunctionType, class OriginState, class Event, class DestinationState, class TransitionFunction, class Guard, typename... TupleTypes>
-inline typename std::enable_if<I < sizeof...(TupleTypes), void>::type
-for_each_row(const std::tuple<OriginState, Event, DestinationState, TransitionFunction, Guard>& row, FunctionType f)
-{
-    std::cout << "implementation" << std::endl;
-	f(row);
-	for_each_row<I + 1, FunctionType, TupleTypes...>(row, f);
-}
-
-
-/*!
- * @brief Backend for a Meta State Machine.
- * When you intheirit this class make sure you:
- * -
-*/
-template<class ...Rows>
-struct MSM
-{
-	template <class Event>
-	void ProcessEvent(const Event& event)
-	{
-        std::cout << "Processing event" << std::endl;
-        int currentState;
-		TransitionState(currentState, event);
-	}
-
-    template <class T>
-    void SetStateActive(const T& newState)
-    {
-        std::cout << "activated state: " << typeid(newState).name() << std::endl;
-    }
-
-    template <class T>
-    void SetStateInactive(const T& stateToInactivate)
-    {
-        std::cout << "activated state: " << typeid(stateToInactivate).name() << std::endl;
-    }
-
-	template<class CurrentState, class Event>
-	void TransitionState(const CurrentState& currentState, const Event& incommingEvent)
-	{
-        std::cout << "Transitioning state" << std::endl;
-		for_each_row([&]<typename ...RowData>(const std::tuple<RowData...>& row, Rows...)
-		{
-			const auto& [originState, event, destinationState, transitionFunction, guard] = row;
-		
-            std::cout << "Checking transitions" << std::endl;
-			if constexpr (currentState == originState && incommingEvent == event)
-			{
-                std::cout << "checking guard" << std::endl;
-				if (guard())
-				{
-					SetStateActive(destinationState);
-					transitionFunction();
-					SetStateInactive(originState);
-                    return;
-				}
-			}
-		});
-        throw std::runtime_error("Transition not implemented!!!");
-	}
-
+    using Type = int;
 };
-
-namespace Guards
-{
-    struct None
-    {
-        bool operator()()
-        {
-            std::cout << "executing guard" << std::endl;
-            return true;
-        }
-    };
-}
-
-bool GuardFunc()
-{
-    return true;
-}
 
 int main()
 {
-    MSM<
-        std::tuple<int, float, double, char, Guards::None>,
-        std::tuple<int, std::string, double, char, Guards::None>,
-        std::tuple<std::string, int, char, float, Guards::None>
-    > stateMachine;
+    std::cout << static_cast<Row<int, None, float>::Type>(5.4) << std::endl;
 
-    float event;
-    stateMachine.ProcessEvent(event);
 
     return 0;
 }
